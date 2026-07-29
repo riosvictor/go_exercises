9497a28a51849f5f398082412cb47140ee04d95a
Test <victorios.ba@gmail.com>
2026-07-29 17:06:52 -0300

    chore: reorg folders

---
+++ go-with-tests/4.dep_injection/caso.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandlerMeuCumprimento))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Cumprimenta(escritor io.Writer, nome string) {
+	fmt.Fprintf(escritor, "Olá, %s", nome)
+}
+
+func HandlerMeuCumprimento(w http.ResponseWriter, r *http.Request) {
+	Cumprimenta(w, "mundo")
+}
